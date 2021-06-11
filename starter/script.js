78c92b6c9bfe308d3715ea54735bb098b2bcec3e
syed <novelcode0@gmail.com>
2021-06-11 10:46:32 +0530

    Lec 250 Chaining Promises

--- starter/script.js
+++ starter/script.js
@@ -121,9 +121,20 @@ setTimeout(() => {
 // };
 
 const getCountryData2 = function (country) {
+  // Country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData2('portugal');
+getCountryData2('argentina');
