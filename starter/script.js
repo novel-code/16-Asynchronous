3ae7a7fde6db9e830292c4b15e31306294ca9c06
syed <novelcode0@gmail.com>
2021-06-21 23:23:08 +0530

    promises in parallel

--- starter/script.js
+++ starter/script.js
@@ -410,7 +410,7 @@ createImage('img/img-1.jpg')
     currentImg.style.display = 'none';
   })
   .catch(err => console.error(err));
-*/
+
 // Lec 258 Async/await
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -480,5 +480,40 @@ console.log('1: Will get location');
 // } catch (err) {
 //   alert(err.message);
 // }
-
+*/
 // lec 260 returning values from async await
+
+// lec 261 Running Promise in Paralle
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'egypt');
